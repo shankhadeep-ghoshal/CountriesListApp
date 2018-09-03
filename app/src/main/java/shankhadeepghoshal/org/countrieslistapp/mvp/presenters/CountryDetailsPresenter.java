75d4b3b435418d9d56ed3c1ce4ac83c8d680332f
shankhadeep-ghoshal <groza.henrymarvin@gmail.com>
2018-09-03 23:10:58 +0300

    Code cleanup: deleted unnecessary comments and imports

--- app/src/main/java/shankhadeepghoshal/org/countrieslistapp/mvp/presenters/CountryDetailsPresenter.java
+++ app/src/main/java/shankhadeepghoshal/org/countrieslistapp/mvp/presenters/CountryDetailsPresenter.java
@@ -4,13 +4,12 @@ import android.support.annotation.NonNull;
 
 import javax.inject.Inject;
 
-import io.reactivex.Flowable;
 import io.reactivex.Maybe;
 import io.reactivex.MaybeObserver;
 import io.reactivex.disposables.Disposable;
 import shankhadeepghoshal.org.countrieslistapp.mvp.models.entities.CountriesFullEntity;
-import shankhadeepghoshal.org.countrieslistapp.mvp.view.CountryDetailsView;
 import shankhadeepghoshal.org.countrieslistapp.mvp.models.repository.countrydetails.CountryDetailsRepository;
+import shankhadeepghoshal.org.countrieslistapp.mvp.view.CountryDetailsView;
 
 public class CountryDetailsPresenter extends BasePresenter<CountryDetailsView> implements
         MaybeObserver<CountriesFullEntity> {
@@ -49,29 +48,4 @@ public class CountryDetailsPresenter extends BasePresenter<CountryDetailsView> i
     public void onComplete() {
         if(!this.disposable.isDisposed()) this.disposable.dispose();
     }
-
-/*
-    @Override
-    public void onNext(CountriesFullEntity countriesFullEntity) {
-        getInjectedView().onLoadParticularCountryData(countriesFullEntity);
-        this.disposable.cancel();
-    }
-
-    @Override
-    public void onError(Throwable e) {
-        getInjectedView().onErrorEncountered(e.getMessage());
-    }
-
-    @Override
-    public void onComplete() {
-        if(this.disposable!=null) disposable.cancel();
-    }
-
-    @Override
-    public void onSubscribe(Subscription d) {
-        // TODO: Lets think of what to do
-        this.disposable = d;
-        d.request(Long.MAX_VALUE);
-    }
-*/
 }
